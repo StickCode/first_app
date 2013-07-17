45bdcf7167182ba73f4a05617cea8625daac6f5c
StickCode <manuel@netixia.fr>
2013-07-17 21:29:43 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '3612f6610cf9d8e9162c79fb381a312a82a19f9394b41b3f71a3beb628d368274f33f79c740b3ebccc2152e882295f1dd1cd389d2c908230bfcbdb600cecb1c9'
